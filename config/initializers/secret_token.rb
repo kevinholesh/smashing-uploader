adb5a0e431ccd6e1df627f8f35fc86947eba49be
Kevin Holesh <me@kevinholesh.com>
2013-08-02 10:52:12 -0400

    Skeleton rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmashingUploader::Application.config.secret_token = '8328a02626b2f9d5c2b38ce2935dc1b54ca68fe88b0930179f1c3494cd0b3eb1ee379f4b1e9ad22f43546ca33bdf9f6151f16dc64fd93d64a73228c914a5e068'
