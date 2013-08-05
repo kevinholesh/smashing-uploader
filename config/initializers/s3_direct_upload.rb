bfc4c781b4a6d08b3c8f218900fc1c4f2cb69bc7
Kevin Holesh <me@kevinholesh.com>
2013-08-05 13:05:27 -0400

    Fixing key names

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -1,5 +1,5 @@
 S3DirectUpload.config do |c|
-  c.access_key_id = ENV['S3_ACCESS_KEY_ID']
-  c.secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+  c.access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
   c.bucket = ENV['FOG_DIRECTORY']
 end
