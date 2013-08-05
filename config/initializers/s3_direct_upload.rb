f2fded25d80fb6f49fab677e8c71ffd2b30297c7
Kevin Holesh <me@kevinholesh.com>
2013-08-05 12:51:46 -0400

    Using local env for s3 keys

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -1,5 +1,5 @@
 S3DirectUpload.config do |c|
-  c.access_key_id = ""
-  c.secret_access_key = ""
-  c.bucket = "smashing-uploader"
+  c.access_key_id = ENV['S3_ACCESS_KEY_ID']
+  c.secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+  c.bucket = ENV['FOG_DIRECTORY']
 end
